19c33b6721092aa10adf59c81ad6fe947fa99333
madina <madiismail2108@gmail.com>
2026-01-12 15:24:47 +0500

    serializers

--- app/models.py
+++ app/models.py
@@ -37,4 +37,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Image(models.Model):
+    product = models.ForeignKey(
+        Product, 
+        related_name= 'images', 
+        on_delete= models.CASCADE
+    )
+    image = models.ImageField(upload_to='product_images/')
 # Create your models here.
--- app/serializers.py
+++ app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Car, Category, Product
+from .models import Car, Category, Product, Image
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,11 +9,33 @@ class CarSerializer(serializers.ModelSerializer):
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'slug', 'parent']
+        fields = ['id', 'name']
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['id', 'image']
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     category_id = serializers.IntegerField(write_only=True)
+    images = ImageSerializer(many=True, read_only=True)
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = [ 'id', "name", "price", "category", "images" ]
+
+    def get_category(self, obj):
+        return {
+            "id": obj.category.id,
+            "name": obj.category.name
+        }
+
+    def get_images(self, obj):
+        return [
+            {
+                "id": image.id,
+                "image": image.image.url
+            }
+            for image in obj.images.all()
+        ]
+
